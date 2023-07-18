3591b6d096fd4adf5f907be41ca169a247ad8b9b
김기현 <creeps1104@gmail.com>
2023-07-18 16:29:04 +0900

    Initial Commit

---
+++ 스유/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  스유
+//
+//  Created by 김기현 on 2023/07/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
